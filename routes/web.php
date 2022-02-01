fca0205a77a3d99374fa0e69dd13e9b607dfc87d
NikitaLejnev <catassasin331@gmail.com>
2022-02-01 09:14:25 +0300

    Created TasksController.php

--- routes/web.php
+++ routes/web.php
@@ -90,7 +90,7 @@ use Illuminate\Support\Facades\Route;
 <!-- Route::get('members/{id}', 'MembersController@show')->name('members.show'); -->
 
 <!-- // Linking the route in a view using the route() helper: -->
-<!-- <a href="<?php echo route('members.show', ['id' => 14]); ?>"> -->
+<!-- <a href="?php echo route('members.show', ['id' => 14]); ?>"> -->
 
 <!-- Example 3-10. Defining a route group
 
