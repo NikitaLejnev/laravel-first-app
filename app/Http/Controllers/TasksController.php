70db79cdf3fa9cbb8ff922885dc0b143f432d41f
NikitaLejnev <catassasin331@gmail.com>
2022-02-01 09:16:17 +0300

    Added Example 3-22

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
-    //
+    // Example 3-22. Simple controller
+
+    // public function index()
+    // {
+    //     return 'Hello, World!';
+    // }
 }
